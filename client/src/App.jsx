f5dffa71a915d672c26dfea40cc08517ca80b1c9
Maratangjoa <xpxdfw4989@gmail.com>
2026-05-19 11:59:17 +0900

    result.jsx 병합

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,5 +1,4 @@
 import { useState } from 'react'
-import { Routes, Route, Navigate } from 'react-router-dom' // 💡 여기에 있던 BrowserRouter 지우기!
 import Login from './pages/Login.jsx'
 import Register from './pages/Register.jsx'
 import MainLobby from './pages/MainLobby.jsx'
