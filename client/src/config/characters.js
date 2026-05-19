713d36ca89bb13e70dc7ee4e3f5984861237bbd2
김린아 <xpxdfw4989@gmail.com>
2026-05-19 17:17:00 +0900

    Dev (#12)
    * config 파일 추가

---
+++ client/src/config/characters.js
@@ -0,0 +1,24 @@
+// config/characters.js
+export const CHARACTER_CONFIG = {
+    korean_shorthair: {
+        character_name: '코리안숏헤어',
+        max_exp: { lv1: 50, lv2: 100, lv3: 150 },
+        unlock_condition: null  // 기본 제공
+    },
+    russian_blue: {
+        character_name: '러시안블루',
+        max_exp: { lv1: 80, lv2: 130, lv3: 180 },
+        unlock_condition: {
+            prev_character: 'korean_shorthair',
+            badge: null
+        }
+    },
+    munchkin: {
+        character_name: '먼치킨',
+        max_exp: { lv1: 110, lv2: 160, lv3: 210 },
+        unlock_condition: {
+            prev_character: 'russian_blue',
+            badge: null //업적 기능 구현시 +'streak_30'
+        }
+    }
+}
